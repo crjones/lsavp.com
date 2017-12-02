e754e98f515cc85c68e68fdf277f703760245670
Christopher Jones <crjones@gmail.com>
2017-12-02 15:24:05 -0600

    Init
    Initial Commit

---
+++ js/init.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
